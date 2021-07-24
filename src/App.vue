486c803a8c12f605a12b047da6f7b66c6997dc23
Steven Sarria <ssarr012@fiu.edu>
2021-07-24 14:46:00 -0400

    fetchWeather function
    Created fetchWeather function

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <main>
       <div>
         <div class='search-box'>
-          <input type='text' class='search-box' placeholder='Search...' />
+          <input type='text' class='search-box' placeholder='Search...' v-model='query' @keypress='fetchWeather'/>
         </div>
 
         <div class='weather-wrap'>
@@ -31,8 +31,22 @@ export default {
   data () {
     return {
       api_key: '604ee7c237024cd80c154355a054b83b',
-      url_base: 'https://api.openweathermap.org/data/2.5'
+      url_base: 'https://api.openweathermap.org/data/2.5',
+      query: '',
+      weather: {},
     }
+  },
+  methods: {
+    fetchWeather (e){
+      if (e.key == 'Enter')
+        fetch(`${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+    }
+  },
+  setResults(results) {
+    this.weather = results;
   }
 }
 </script>
