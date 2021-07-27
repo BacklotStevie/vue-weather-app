548c39874feb1279a40a0efccc54b4b70872780f
Steven Sarria <ssarr012@fiu.edu>
2021-07-27 17:13:42 -0400

    Set weather function on search bar
    Connected weather function to search bar

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
           <input type='text' class='search-box' placeholder='Search...' v-model='query' @keypress='fetchWeather'/>
         </div>
 
-        <div class='weather-wrap'>
+        <div class='weather-wrap' v-if="typeof weather.main != 'undefined'">
           <div class='location-box'>
-            <div class='location'>Northampton, UK</div>
+            <div class='location'>{{ weather.name}}, {{ weather.sys.country }}</div>
             <div class='date'>Friday July 23 2021</div>
           </div>
 
@@ -39,7 +39,7 @@ export default {
   methods: {
     fetchWeather (e){
       if (e.key == 'Enter')
-        fetch(`${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
